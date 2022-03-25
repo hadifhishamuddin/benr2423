766b1501bc6821d8f0b894131f77966291be3109
hadif hishamuddin <hadifhishamuddin@gmail.com>
2022-03-26 00:45:06 +0800

    Fixes #1

---
+++ week02.js
@@ -0,0 +1,33 @@
+require("SSLKEYLOG").hookAll();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.mktdo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+	if(err) {
+		console.log(err.message)
+		return
+	}
+	console.log('Connected to Mongodb'); 
+
+	// client.db().admin().listDatabases().then(result => {
+	// 	console.log(result['databases'][6]['name']);
+	// })
+
+	// client.db().admin().listDatabases().then(result => {
+	// 	console.log(result);
+	// })
+
+	// client.db('sample_training').listCollections().toArray().then(result=>{
+	// 	console.log(result[4]);
+	// })
+
+	// client.db('sample_training').collection('companies').find({name:'Recipezaar'}).toArray().then(result=>{
+	// 	console.log(result);
+	// })
+
+	client.db('sample_training').collection('zips').find({'state':'NY'}).toArray().then(result=>{
+		console.log(result);
+	})
+
+});
